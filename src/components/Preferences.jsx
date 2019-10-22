ce8256c8e4e6fa37fd63d79c55d4c580f2242c82
hemeshvpatel <hemesh.v.patel@gmail.com>
2019-10-22 11:42:35 -0500

    navbar with link placeholders for pref and logout working

--- src/components/Preferences.jsx
+++ src/components/Preferences.jsx
@@ -2,7 +2,22 @@ import React, { Component } from "react";
 
 class Preferences extends Component {
   render() {
-    return <h1>Preferences Page</h1>;
+    return (
+      <div>
+        <h1>Your Preferences:</h1>
+        <div class="form-check">
+          <input
+            class="form-check-input"
+            type="checkbox"
+            value=""
+            id="defaultCheck1"
+          />
+          <label class="form-check-label" for="defaultCheck1">
+            Cuisine Types
+          </label>
+        </div>
+      </div>
+    );
   }
 }
 
