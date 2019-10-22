91cc189eb594bb8b722fa656f9aa08f639033e91
hemeshvpatel <hemesh.v.patel@gmail.com>
2019-10-22 16:03:19 -0500

    google api fetch working returning randmom match

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Home from './components/Home'
-import Signup from './components/Signup'
-import Login from './components/Login'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-
-
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./components/Home";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <Router>
-      <div>
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/signup" component={Signup} />
-        <Route exact path="/login" component={Login} />
-      </div>
-    </Router>
+        <div>
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/login" component={Login} />
+        </div>
+      </Router>
     );
   }
-  
 }
 
 export default App;
