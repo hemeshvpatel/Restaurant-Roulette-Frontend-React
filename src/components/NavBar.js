094225c3e24f0e86f365825b46ccbe29389c1624
hemeshvpatel <hemesh.v.patel@gmail.com>
2019-10-22 11:03:30 -0500

    updated to pills with margin of 2

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,14 +3,14 @@ import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <ul class="nav justify-content-end">
+    <ul class="nav nav-pills justify-content-end">
       <li class="nav-item">
-        <NavLink class="nav-link active" to="/preferences">
+        <NavLink class="nav-link active m-2" to="/preferences">
           Preferences
         </NavLink>
       </li>
       <li class="nav-item">
-        <NavLink class="nav-link active" to="/logout">
+        <NavLink class="nav-link active m-2" tabindex="-1" to="/logout">
           Logout
         </NavLink>
       </li>
