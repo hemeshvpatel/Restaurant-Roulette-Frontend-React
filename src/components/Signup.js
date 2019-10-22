9c8dc1899243d4d89248d58dd17067856dd9eb1f
Syd <green@Syds-MacBook-Air-2.local>
2019-10-22 10:44:15 -0500

    updated sign up experience to include continue and back buttons

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -13,7 +13,8 @@ export default class Signup extends Component {
             password:  '',
             password_confirmation: '',
             zipcode: 0,
-            radius: 0
+            radius: 0,
+            preferences: []
         }
     }
 
@@ -47,7 +48,57 @@ export default class Signup extends Component {
             console.log(response)
         })
     }
+
+    // handleCheckBoxSubmit = (event) => {
+    //     fetch request to cuisine preferences - post method. 
+    //     save all preferences into an array in state - then
+    //     .map through the array and do a fetch request to database for each cuisine
+    // }
     
+    next() {
+        let currentStep = this.state.currentStep
+        currentStep = currentStep >= 1? 2: currentStep + 1
+        this.setState({
+            currentStep: currentStep
+        })
+    }
+
+    prev() {
+        let currentStep = this.state.currentStep
+        currentStep = currentStep <= 1? 1: currentStep - 1
+        this.setState({
+            currentStep: currentStep
+        })
+    }
+
+    get previousButton(){
+        let currentStep = this.state.currentStep;
+        if(currentStep !==1){
+          return (
+            <button 
+                className="btn btn-secondary" 
+                type="button" onClick={this.prev}>
+                Previous
+            </button>
+          )
+        }
+        return null;
+      }
+      
+      get nextButton(){
+        let currentStep = this.state.currentStep;
+        if(currentStep <2){
+          return (
+            <button 
+                className="btn btn-primary float-right" 
+                type="button" onClick={this.next}>
+                Next
+            </button>        
+          )
+        }
+        return null;
+      }
+
     render() {
         return(
             <div>
@@ -59,14 +110,20 @@ export default class Signup extends Component {
                     <Step1 
                     currentStep={this.state.currentStep} 
                     handleChange={this.handleChange}
+                    name={this.state.name}
                     email={this.state.email}
+                    password={this.state.password}
+                    password_confirmation={this.state.password_confirmation}
                     />
                     <Step2 
+                    preferences={this.state.preferences}
                     currentStep={this.state.currentStep} 
                     handleChange={this.handleChange}
-                    username={this.state.username}
-                    />      
-
+                    zipcode={this.state.zipcode}
+                    radius={this.state.radius}
+                    />
+                    {this.previousButton}
+                    {this.nextButton}      
                 </form>
                 </React.Fragment>
             </div>
