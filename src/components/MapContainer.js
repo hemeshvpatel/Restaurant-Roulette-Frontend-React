00b9763dc5d91d704faa486dc582fa804bdce40d
Syd <green@Syds-MacBook-Air-2.local>
2019-12-05 11:31:57 -0600

    attempted to fix lng with no success - added like button to set up with db

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,39 +1,61 @@
-import React, { Component } from 'react';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import React, { Component } from "react";
+import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
+import { Button, Icon, Label } from "semantic-ui-react";
 
 const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
 
-
 class MapContainer extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            latData: '',
-            longData: ''
-        }
-    }
+    this.state = {
+      latData: "",
+      longData: ""
+    };
+  }
 
-    componentDidMount() {
-        this.setState({ 
-            latData : this.props.latData,
-            longData: this.props.longData
-        })
-    }
+  componentDidMount() {
+    this.setState({
+      latData: this.props.latData,
+      longData: this.props.longData
+    });
+  }
 
-    render() {
-        return (
-            <Map
-            style={{ width: '75%', height: '75%' }}
-            google={this.props.google}
-            zoom={18}
-            center={{ lat: parseFloat(this.props.latData), lng: parseFloat(this.props.longData) }} >
-                <Marker position={{ lat: parseFloat(this.props.latData), lng: parseFloat(this.props.longData) }} />
-            </Map>
-        )
-    }
-};
+  render() {
+    return (
+      <>
+        <div>
+          <Button as="div" labelPosition="right" style={{ width: auto }}>
+            <Button color="pink">
+              <Icon name="heart" />
+              Like
+            </Button>
+            <Label as="a" basic color="pink" pointing="left">
+              2,048
+            </Label>
+          </Button>
+        </div>
+        <Map
+          style={{ width: "75%", height: "75%" }}
+          google={this.props.google}
+          zoom={18}
+          center={{
+            lat: parseFloat(this.props.latData),
+            lng: parseFloat(this.props.longData)
+          }}
+        >
+          <Marker
+            position={{
+              lat: parseFloat(this.props.latData),
+              lng: parseFloat(this.props.longData)
+            }}
+          />
+        </Map>
+      </>
+    );
+  }
+}
 
 export default GoogleApiWrapper({
-    apiKey: apiKey
-  })(MapContainer);
+  apiKey: apiKey
+})(MapContainer);
